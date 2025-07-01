6e827d090f6f1ab7f34814614ba80c564643608b
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2025-07-01 13:04:51 -0400

    remove `any`

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -13,7 +13,7 @@ interface BrowserWindow extends Window {
 ;(async () => {
   try {
     const browserCore = await import('@datadog/browser-core')
-    browserCore.defineGlobal(browserCore.getGlobalObject(), 'DD_FLAGGING' as any, { Provider: DatadogProvider })
+    browserCore.defineGlobal(browserCore.getGlobalObject(), 'DD_FLAGGING' as keyof typeof globalThis, { Provider: DatadogProvider })
   } catch (error) {
     // @datadog/browser-core not available, skip global registration
     // This is expected when the library is used without the RUM integration
