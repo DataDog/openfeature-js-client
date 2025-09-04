22491b8b395ad933a8a6c595caa4433cadbedeb1
Tyler Potter <tyler.potter@datadoghq.com>
2025-09-04 13:36:26 -0600

    fix: browser-core dep (#28)
    * fix for latest browser-core update

--- packages/browser/src/transport/startExposuresBatch.ts
+++ packages/browser/src/transport/startExposuresBatch.ts
@@ -1,5 +1,11 @@
 import type { Context, PageMayExitEvent, RawError } from '@datadog/browser-core'
-import { createIdentityEncoder, startBatchWithReplica, Observable } from '@datadog/browser-core'
+import {
+  createIdentityEncoder,
+  createFlushController,
+  createBatch,
+  createHttpRequest,
+  Observable,
+} from '@datadog/browser-core'
 import type { FlaggingConfiguration } from '../domain/configuration'
 
 export function startExposuresBatch(
@@ -7,19 +13,18 @@ export function startExposuresBatch(
   reportError: (error: RawError) => void,
   pageMayExitObservable: Observable<PageMayExitEvent>
 ) {
-  const batch = startBatchWithReplica(
-    configuration,
-    {
-      endpoint: configuration.exposuresEndpointBuilder,
-      encoder: createIdentityEncoder(),
-    },
-    // No replica for now
-    undefined,
-    reportError,
-    pageMayExitObservable,
-    // We don't track sessions, so no session expiration for now
-    new Observable()
-  )
+  const batch = createBatch({
+    encoder: createIdentityEncoder(),
+    request: createHttpRequest([configuration.exposuresEndpointBuilder], configuration.batchBytesLimit, reportError),
+    flushController: createFlushController({
+      messagesLimit: configuration.batchMessagesLimit,
+      bytesLimit: configuration.batchBytesLimit,
+      durationLimit: configuration.flushTimeout,
+      pageMayExitObservable,
+      sessionExpireObservable: new Observable(),
+    }),
+    messageBytesLimit: configuration.messageBytesLimit,
+  })
 
   return batch
 }
