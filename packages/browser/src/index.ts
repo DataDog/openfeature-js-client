a8b56decfadf1cd14edefdc1d19ae8e884bd7dd3
Tyler Potter <tyler.potter@datadoghq.com>
2025-09-15 11:23:10 -0600

    fix: export FlaggingInitConfiguration (#36)

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -3,6 +3,7 @@ import { DatadogProvider } from './openfeature/provider'
 
 export { DatadogProvider }
 export { configurationFromString, configurationToString } from '@datadog/flagging-core'
+export { type FlaggingInitConfiguration } from './domain/configuration'
 
 // Build environment placeholder for testing
 const SDK_VERSION = __BUILD_ENV__SDK_VERSION__
