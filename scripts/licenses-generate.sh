25a02bacd50beebf51db8e28e636d31942e7cc98
Tyler Potter <tyler.potter@datadoghq.com>
2025-12-11 13:36:14 -0700

    simplify

--- scripts/licenses-generate.sh
+++ scripts/licenses-generate.sh
@@ -7,30 +7,15 @@ PYTHON_VERSION="3.11.12"
 ### Requires python 3.11.12 and dd-license-attribution
 # see https://github.com/DataDog/dd-license-attribution for installation details
 
-### Make sure python version is 3.11.12
-if command -v pyenv &> /dev/null; then
-    # Use pyenv to set the Python version
-    if ! pyenv versions | grep "${PYTHON_VERSION}" > /dev/null; then
-        echo "Python ${PYTHON_VERSION} is not installed via pyenv"
-        read -p "Would you like to install Python ${PYTHON_VERSION} now? (y/n) " -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            echo "Installing Python ${PYTHON_VERSION}..."
-            pyenv install ${PYTHON_VERSION}
-        else
-            echo "Please install Python ${PYTHON_VERSION} manually: 'pyenv install ${PYTHON_VERSION}'"
-            exit 1
-        fi
+### Check Python version
+if ! python --version 2>&1 | grep -q "${PYTHON_VERSION}"; then
+    echo "ERROR: Python version ${PYTHON_VERSION} is required"
+    echo "Current version: $(python --version 2>&1)"
+    echo "Please install Python ${PYTHON_VERSION}"
+    if command -v pyenv &> /dev/null; then
+        echo "  Using pyenv: pyenv install ${PYTHON_VERSION} && pyenv local ${PYTHON_VERSION}"
     fi
-    pyenv local ${PYTHON_VERSION}
-    echo "Set Python version to ${PYTHON_VERSION} using pyenv"
-else
-    # Fallback: check if the correct version is available
-    if ! python --version | grep "${PYTHON_VERSION}" > /dev/null; then
-        echo "Python version is not ${PYTHON_VERSION}"
-        echo "Please install python ${PYTHON_VERSION} or use pyenv: 'pyenv install ${PYTHON_VERSION} && pyenv local ${PYTHON_VERSION}'"
-        exit 1
-    fi
+    exit 1
 fi
 
 if ! command -v dd-license-attribution &> /dev/null; then
