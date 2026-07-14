e163e9bf060aec1c712eae2c2ab301f71aa30e1e
Sameeran Kunche <sameeran.kunche@datadoghq.com>
2026-07-14 08:11:30 -0700

    Move browser evaluation helper to core

--- packages/browser/test/evaluation.spec.ts
+++ packages/browser/test/evaluation.spec.ts
@@ -1,7 +1,6 @@
-import { type FlagsConfiguration, OperatorType } from '@datadog/flagging-core'
+import { evaluate, type FlagsConfiguration, OperatorType } from '@datadog/flagging-core'
 import { configurationFromString } from '@datadog/flagging-core/rules-based'
 import type { ErrorCode } from '@openfeature/web-sdk'
-import { evaluate } from '../src/evaluation'
 import configurationWire from './data/precomputed-v1-wire.json'
 
 const configuration = configurationFromString(
