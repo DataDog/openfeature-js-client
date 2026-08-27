2c9e9fceba7516066287684ef728efa221f98570
Sameeran Kunche <sameeran.kunche@datadoghq.com>
2026-08-27 13:50:51 -0700

    docs(core): attach internal tag to parser definition

--- packages/core/src/configuration/index.ts
+++ packages/core/src/configuration/index.ts
@@ -10,5 +10,4 @@ export {
   configurationToPrecomputedString as configurationToString,
 } from './precomputed-wire'
 export type { FlagsConfigurationWire } from './wire-types'
-/** @internal */
 export { parsePrecomputedConfigurationResponse } from './wire-validation'
