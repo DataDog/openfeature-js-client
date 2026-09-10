04d7a48229486a7b3ff04640fe37086ae85a4e53
Sameeran Kunche <sameeran.kunche@datadoghq.com>
2026-09-10 11:04:05 -0700

    test(browser): cover rules fetcher entrypoint export

--- packages/browser/test/rules-based-entrypoint.spec.ts
+++ packages/browser/test/rules-based-entrypoint.spec.ts
@@ -1,9 +1,10 @@
-import { DatadogOfflineProvider, DatadogProvider } from '../src/rules-based'
+import { DatadogOfflineProvider, DatadogProvider, fetchRulesConfiguration } from '../src/rules-based'
 
 describe('rules-based entry point', () => {
   it('exports and registers rules-based browser APIs', () => {
     expect(DatadogProvider).toBeDefined()
     expect(DatadogOfflineProvider).toBeDefined()
+    expect(fetchRulesConfiguration).toBeDefined()
     expect((globalThis as { DD_FLAGGING?: { OfflineProvider?: unknown } }).DD_FLAGGING?.OfflineProvider).toBe(
       DatadogOfflineProvider
     )
